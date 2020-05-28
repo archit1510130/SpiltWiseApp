d5f9acadd23ee7b4d1322085ad853fcd306af172
archit1510130 <archit.agrawal.003@gmail.com>
2020-05-28 19:21:02 +0530

    first commit

---
+++ SpilitWise/src/main/java/SplitApp/App.java
@@ -0,0 +1,16 @@
+package SplitApp;
+
+import lombok.Data;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
